7f9f5b79b3f330189cd94be7a134cdd8b68e9d0a
Raijjjinn <emhutch003@gmail.com>
2024-01-26 12:03:35 -0500

    added skeleton file for protocols

---
+++ protocol_template.py
@@ -0,0 +1,18 @@
+from opentrons import protocol_api
+from opentrons import types
+
+metadata = {
+    "protocolName":"Example Protocol",
+    "author":"John Doe",
+    "description":"Insert a brief description of what the protocol intends to do",
+}
+
+requirements = {"robotType": "OT-2", "apiLevel": "2.16"}
+
+def run(protocol: protocol_api.ProtocolContext):
+    #----load in labware here----# 
+
+
+    #----load in pipette(s) here----#
+
+    #----instructions for OT-2 here----#
